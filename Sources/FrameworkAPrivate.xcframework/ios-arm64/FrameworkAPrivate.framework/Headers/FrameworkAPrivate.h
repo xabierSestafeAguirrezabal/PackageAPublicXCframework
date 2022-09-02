7a7356a361dd49638f109d67dc206ce7c286a1f1
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-02 10:50:57 +0200

    Initial Commit

---
+++ Sources/FrameworkAPrivate.xcframework/ios-arm64/FrameworkAPrivate.framework/Headers/FrameworkAPrivate.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkAPrivate.h
+//  FrameworkAPrivate
+//
+//  Created by Jose F Gómez Arbaizar on 2/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkAPrivate.
+FOUNDATION_EXPORT double FrameworkAPrivateVersionNumber;
+
+//! Project version string for FrameworkAPrivate.
+FOUNDATION_EXPORT const unsigned char FrameworkAPrivateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkAPrivate/PublicHeader.h>
+
+
